6c316e1774c03be5b922ce7ed2546c63f2d9f1fc
jaidenagrimminck <jaidng1@gmail.com>
2025-02-20 14:45:58 +0100

    fix background color

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{
+          backgroundColor: "#0a0a0a"
+        }}
       >
         {children}
       </body>
