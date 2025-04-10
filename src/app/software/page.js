ae69b17bdf8fdd85b2bbd8490b92c53d851e222d
jaidenagrimminck <jaidng1@gmail.com>
2025-04-10 14:30:44 +0200

    update w/ three js

--- src/app/software/page.js
+++ src/app/software/page.js
@@ -1,9 +1,36 @@
+import React from "react";
+import { Geist, Geist_Mono, Krona_One, Rubik_Mono_One } from "next/font/google";
+
+
+const kronaOne = Krona_One({
+    variable: "--font-krona-one",
+    subsets: ["latin"],
+    weight: "400",
+});
+
+const rubikMonoOne = Rubik_Mono_One({
+    variable: "--font-rubik-mono-one",
+    subsets: ["latin"],
+    weight: "400",
+});
 
 export default function SoftwarePage() {
     return (
-        <div>
-            <h1>Software</h1>
-            <p>This is the software page.</p>
+        <>
+        <div className="flex flex-col items-center w-full mt-[30px] mb-[30px]">
+            <h1 className={"text-4xl " + rubikMonoOne.className}>Software</h1>
         </div>
+        <div className="flex flex-col items-center w-full">
+            <div className="mb-[10px]">
+                <h2 className={"text-2xl " + rubikMonoOne.className}>JPather</h2>
+            </div>
+            <div className="mb-[10px]">
+                <h2 className={"text-2xl " + rubikMonoOne.className}>JScout</h2>
+            </div>
+            <div className="mb-[10px]">
+                <h2 className={"text-2xl " + rubikMonoOne.className}>JSim</h2>
+            </div>
+        </div>
+        </>
     );
 }
--- src/modules/ImageHover.js
+++ src/modules/ImageHover.js
@@ -7,13 +7,13 @@ export default function ImageHover({ children, href, width, height }) {
             <div className="flex justify-center items-center w-full h-full bg-black bg-opacity-0 hover:bg-opacity-50 transition duration-300 image-hover" onClick={() => window.open(href, '_blank')}>
                 {children}
                 {
-                // <div style={{
-                //     width: `${width}px`,
-                //     height: `${height}px`,
-                //     //backgroundColor: 'rgba(0, 0, 0, 0.5)',
-                // }} className={`text-white text-2xl absolute flex justify-center items-center top-0 left-0 font-bold w-[${width}px] h-[${height}px]`}>
-                //     <span>Click Me</span>
-                // </div>
+                <div style={{
+                    width: `${width}px`,
+                    height: `${height}px`,
+                    //backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                }} className={`text-white text-2xl absolute flex justify-center items-center top-0 left-0 font-bold w-[${width}px] h-[${height}px]`}>
+                    <span>Click Me</span>
+                </div>
                 }
             </div>
         </div>
